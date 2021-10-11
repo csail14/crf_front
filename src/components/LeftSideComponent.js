ed9faf59aa55f8e1729e61d197e8de77e99c45fa
Camille <cssaillard@gmail.com>
2021-10-11 17:10:54 +0200

    test mise en page

--- src/components/LeftSideComponent.js
+++ src/components/LeftSideComponent.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
+import logoBandeauCroixRouge from "../assets/logoBandeauCroixRouge.svg";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const ImageContainer = styled.div`
+  margin: 22px 40px;
+  cursor: pointer;
+`;
+
+const MainContainer = styled.div`
+  min-height: 100vh;
+  background-color: #f7f9fa;
+`;
 
 const LeftSideComponent = (props) => {
-  return <div>LeftSideComponent</div>;
+  console.log("props", window.history);
+  return (
+    <MainContainer>
+      <ImageContainer>
+        <Link to="/home">
+          <img src={logoBandeauCroixRouge} alt="logoBandeauCroixRouge" />
+        </Link>
+      </ImageContainer>
+    </MainContainer>
+  );
 };
 
 const mapDispatchToProps = {};
