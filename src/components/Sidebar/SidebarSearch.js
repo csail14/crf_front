5ea698ce78c1a17762526407f59efcf499e0fd4c
Camille <cssaillard@gmail.com>
2021-11-26 14:09:32 +0100

    press enter goes to search

--- src/components/Sidebar/SidebarSearch.js
+++ src/components/Sidebar/SidebarSearch.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { GoSearch } from "react-icons/all";
 import { Link } from "react-router-dom";
-
+import { useHistory } from "react-router-dom";
 const SidebarSearch = (props) => {
   const handleChange = (e) => {
     props.loadKeywordsFilter(e.target.value);
   };
+  let history = useHistory();
 
   return (
     <div className={"form_container"}>
-      <form className={"sidebar_search"}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          history.push("/recherche/");
+        }}
+        className={"sidebar_search"}
+      >
         <div className={"sidebar_input"}>
           <input
             type="text"
@@ -22,7 +29,7 @@ const SidebarSearch = (props) => {
           onClick={() => props.setShowMenu(false)}
           className={"sidebar_icon"}
         >
-            <GoSearch />
+          <GoSearch />
         </Link>
       </form>
     </div>
