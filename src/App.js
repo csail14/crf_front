92c91d2abfd48c37c0f49a4f638afcf47a48fd57
Estelle <estelle.courdoisy@gmail.com>
2021-12-12 22:29:39 +0100

    remove okta test

--- src/App.js
+++ src/App.js
@@ -73,21 +73,21 @@ function App(props) {
               oktaAuth={oktaAuth}
               restoreOriginalUri={restoreOriginalUri}
             >
-              <SecureRoute exact path="/" component={HOC(Home)} />
-              <SecureRoute exact path="/home" component={HOC(Home)} />
-              <SecureRoute
+              <Route exact path="/" component={HOC(Home)} />
+              <Route exact path="/home" component={HOC(Home)} />
+              <Route
                 exact
                 path="/indicateurs/:id"
                 component={HOC(Indicateur)}
               />
-              <SecureRoute
+              <Route
                 exact
                 path="/domaine-impact/:id"
                 component={HOC(Indicateur)}
               />
-              <SecureRoute exact path="/articles/:id" component={HOC(Article)} />
-              <SecureRoute exact path="/documents/:id" component={HOC(Document)} />
-              <SecureRoute exact path="/:id" component={HOC(OtherPage)} />
+              <Route exact path="/articles/:id" component={HOC(Article)} />
+              <Route exact path="/documents/:id" component={HOC(Document)} />
+              <Route exact path="/:id" component={HOC(OtherPage)} />
             </Security>
           </Switch>
           <Footer />
